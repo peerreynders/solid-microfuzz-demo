05dbdb5bcea7096652e865940a129ba0d79201c3
Peer Reynders <17050883+peerreynders@users.noreply.github.com>
2023-07-27 23:52:23 -0400

    Updated Highlight component

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -1,30 +1,35 @@
-import { For, Show, type Component } from 'solid-js';
+import { For, Switch, Match, type Component } from 'solid-js';
 import type { HighlightRanges } from '@nozbe/microfuzz';
 
-type SliceHighlight =
-  | { kind: false; start: number; end: number }
-  | { kind: true; start: number; end: number };
+const SLICE_REGULAR = -1;
+const SLICE_FULL = 0;
+const SLICE_HIGHLIGHT = 1;
 
-function toSlices(ranges: HighlightRanges | undefined, length: number) {
-  if (!ranges) return [{ kind: false, start: 0, end: length + 1 }];
+type SliceRegular = { kind: typeof SLICE_REGULAR; text: string };
+type SliceFull = { kind: typeof SLICE_FULL };
+type SliceHighlight = { kind: typeof SLICE_HIGHLIGHT; text: string };
+type Slice = SliceRegular | SliceFull | SliceHighlight;
+
+const slicesFull: Slice[] = [{ kind: SLICE_FULL }];
+
+function toSlices(text: string, ranges: HighlightRanges | undefined) {
+  if (!ranges || ranges.length < 1) return slicesFull;
 
   let index = 0;
-  const end = length + 1;
-  const slices: SliceHighlight[] = [];
+  const end = text.length + 1;
+  const slices: Slice[] = [];
   for (const [x, y] of ranges) {
-    if (x > index) slices.push({ kind: false, start: index, end: x });
+    if (x > index)
+      slices.push({ kind: SLICE_REGULAR, text: text.slice(index, x) });
 
     index = y + 1;
-    slices.push({ kind: true, start: x, end: index });
+    slices.push({ kind: SLICE_HIGHLIGHT, text: text.slice(x, index) });
   }
-
-  if (end > index) slices.push({ kind: false, start: index, end });
+  if (end > index)
+    slices.push({ kind: SLICE_REGULAR, text: text.slice(index, end) });
 
   return slices;
 }
-
-const toSlice = (text: string, { start, end }: SliceHighlight) =>
-  text.slice(start, end);
 
 type Props = {
   text: string;
@@ -35,13 +40,19 @@ type Props = {
 const Highlight: Component<Props> = (props) => {
   return (
     <>
-      <For each={toSlices(props.ranges, props.text.length)}>
+      <For each={toSlices(props.text, props.ranges)}>
         {(slice) => (
-          <Show when={slice.kind} fallback={toSlice(props.text, slice)}>
-            <span class={props.classItemName}>
-              {toSlice(props.text, slice)}
-            </span>
-          </Show>
+          <Switch>
+            <Match when={slice.kind === SLICE_FULL}>{props.text}</Match>
+            <Match when={slice.kind === SLICE_REGULAR}>
+              {(slice as SliceRegular).text}
+            </Match>
+            <Match when={slice.kind === SLICE_HIGHLIGHT}>
+              <span class={props.classItemName}>
+                {(slice as SliceHighlight).text}
+              </span>
+            </Match>
+          </Switch>
         )}
       </For>
     </>
